545d9cc324afd1b820857f50dc8cc6619199a2a0
akihiromichibata <signus.a.michibata@gmail.com>
2025-12-01 09:35:09 +0900

    Update OneSignalSDKWorker.js

--- frontend/OneSignalSDKWorker.js
+++ frontend/OneSignalSDKWorker.js
@@ -1,1 +1,9 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
+
+self.addEventListener('install', function(event) {
+  self.skipWaiting && self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  self.clients && self.clients.claim && self.clients.claim();
+});
